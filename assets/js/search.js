2b97ea5b6bcb3ab314bc94d1cd510fe40c18a3ab
souad82 <souadslama09@gmail.com>
2022-10-12 16:55:56 +0200

    get results from search bar

---
+++ assets/js/search.js
@@ -0,0 +1,65 @@
+let searchResult = document.querySelector(".container");
+let contactsListElement = document.getElementById("persons-list");
+
+let searchInput = document.getElementById("search-id");
+
+fetch('./data.json')
+    .then(res => res.json())
+    .then(json =>{
+
+        // iterating products
+        for (let value of json){
+            addElement(searchResult, value);
+        }
+        
+    });
+
+// add event listener
+searchInput.addEventListener('keyup', searchmovies);
+
+
+
+
+// callback function 
+function searchmovies(){
+    let searchValue = searchInput.value.toUpperCase();
+    let item = searchResult.querySelectorAll('.card');
+    let b =0;
+    console.log(item.length);
+    for (let i = 0; i < item.length; i++){
+        let span = item[i].querySelector('.title');
+        
+        if(span.innerHTML.toUpperCase().indexOf(searchValue) > -1){
+            item[i].style.display = "initial";
+            b++;
+        }else{
+            item[i].style.display = "none";
+        }
+        
+      }
+
+      document.getElementById("result").innerHTML="Found "+b+" results for '"+searchInput.value+"'";
+}
+
+
+
+// get value from the api create dynamic element
+function addElement(appendIn, value){
+    let div = document.createElement('div');
+    div.className = "card";
+
+    let { thumbnail,title, category } = value;
+
+    div.innerHTML = `
+                <img class="resultImg" src=${thumbnail.regular.small}></img>
+                <h1 class="title">${title}</h1>
+                <a href="#" class="category">${category}</a>
+            
+    `;
+    appendIn.appendChild(div);
+}
+
+
+
+
+
